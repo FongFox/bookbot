042848350422e316e29745cd18cf2f5e06555dfd
FongFox <phong.tgn.coder@gmail.com>
2025-12-01 11:55:56 +0000

    Count character frequencies in book text

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words
+from stats import count_character, count_words
 
 
 def get_book_text(file_path):
@@ -13,8 +13,10 @@ def get_book_text(file_path):
 def main():
     book_contents = get_book_text("books/frankenstein.txt")
     num_words = count_words(book_contents)
+    num_character_dict = count_character(book_contents)
 
     print(f"Found {num_words} total words")
+    print(num_character_dict)
 
 
 # Using the special variable
--- stats.py
+++ stats.py
@@ -2,3 +2,19 @@ def count_words(content):
     words = content.split()
 
     return len(words)
+
+
+def count_character(content):
+    char_count_dict = {}
+
+    for char in content:
+        char_lower = char.lower()
+        if char_lower in char_count_dict:
+            char_count_dict[char_lower] = char_count_dict[char_lower] + 1
+        else:
+            char_count_dict[char_lower] = 1
+
+    # print("==== Check ====")
+    # print(char_count_dict)
+
+    return char_count_dict
