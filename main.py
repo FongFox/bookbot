a8f85d4041686b0424b203da2e6ffceeb9a07b21
FongFox <phong.tgn.coder@gmail.com>
2025-12-01 14:00:36 +0000

    Complete report printing and character sorting

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_character, count_words
+from stats import count_character, count_words, sort_character
 
 
 def get_book_text(file_path):
@@ -10,13 +10,31 @@ def get_book_text(file_path):
     return file_contents
 
 
-def main():
-    book_contents = get_book_text("books/frankenstein.txt")
+def print_report():
+    book_file_path = "books/frankenstein.txt"
+    book_contents = get_book_text(book_file_path)
     num_words = count_words(book_contents)
     num_character_dict = count_character(book_contents)
+    num_character_list = sort_character(num_character_dict)
 
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_file_path}...")
+    print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
-    print(num_character_dict)
+    print("--------- Character Count -------")
+    sort_character(num_character_dict)
+    # print(num_character_dict)
+    # print(num_character_list)
+    for num_char in num_character_list:
+        if num_char["name"].isalpha():
+            print(f"{num_char['name']}: {num_char['num']}")
+        else:
+            continue
+    print("============= END ===============")
+
+
+def main():
+    print_report()
 
 
 # Using the special variable
--- stats.py
+++ stats.py
@@ -14,7 +14,24 @@ def count_character(content):
         else:
             char_count_dict[char_lower] = 1
 
-    # print("==== Check ====")
-    # print(char_count_dict)
-
     return char_count_dict
+
+
+def convert_dict_to_list(item_dict):
+    item_list = []
+    for key in item_dict.keys():
+        temp_dict = {"name": key, "num": item_dict[key]}
+        item_list.append(temp_dict)
+
+    return item_list
+
+
+def sort_on(items):
+    return items["num"]
+
+
+def sort_character(char_count_dict):
+    char_count_list = convert_dict_to_list(char_count_dict)
+    char_count_list.sort(reverse=True, key=sort_on)
+
+    return char_count_list
